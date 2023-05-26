42cc0ae5f8bef6f03439aa98d947aae3b25ce29e
Efonovia <lordnovia@gmail.com>
2023-05-27 00:56:16 +0100

    finalize

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-
+import cheerio from "cheerio"
 // function to get the raw data
 const getRawData = (URL) => {
    return fetch(URL)
@@ -9,12 +9,15 @@ const getRawData = (URL) => {
       });
 }
 
-const URL = "https://en.wikipedia.org/wiki/Cricket_World_Cup";
+const URL = "https://buff.163.com/market/csgo#tab=selling&page_num=1";
 
 // start of the program
 const getSiteInfo = async () => {
-   const RawData = await getRawData(URL);
-   console.log(RawData);
+    const RawData = await getRawData(URL);
+    const parsedData = cheerio.load(RawData)
+
+    const itemArea = parsedData('li, .card_csgo').text()
+    console.log(itemArea);
 }
 
 // invoking the main function
--- test.py
+++ test.py
@@ -1,19 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
 
-htmlText = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(htmlText, 'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-for job in jobs:
-    publishedDate = job.find('span', class_ = 'sim-posted').span.text
-    if 'few' in publishedDate:
-        companyName = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-        skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-        moreInfo = job.header.h2.a["href"]
+htmlText = requests.get('https://buff.163.com/market/csgo#tab=selling&page_num=1').text
+soup = BeautifulSoup(htmlText, 'html.parser')
+itemUL = soup.find('ul')
+print(itemUL.encode('utf-8'))
+# print(itemUL.encode('utf-8', errors="ignore"))
 
-        print(f"COMPANY NAME: {companyName.strip()}")
-        print(f"REQUIRED SKILLS: {skills.strip()}")
-        print(f"LINK TO JOB: {moreInfo}")
 
-        print("")
-        print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+# for job in jobs:
+#     publishedDate = job.find('span', class_ = 'sim-posted').span.text
+#     if 'few' in publishedDate:
+#         companyName = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+#         skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+#         moreInfo = job.header.h2.a["href"]
+
+#         print(f"COMPANY NAME: {companyName.strip()}")
+#         print(f"REQUIRED SKILLS: {skills.strip()}")
+#         print(f"LINK TO JOB: {moreInfo}")
+
+#         print("")
+#         print("")
